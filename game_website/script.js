6621728224acc741d2404231371e9083f6ba7690
scotty <102717@glr.nl>
2024-10-10 22:12:53 +0200

    i cooked till th food turned to diamonds

--- game_website/script.js
+++ game_website/script.js
@@ -1,6 +1,7 @@
 // Select all game elements and overlay elements
 const gameElements = document.querySelectorAll(".game_1, .game_2, .game_3");
 const overlayElements = document.querySelectorAll(".overlay");
+const navbar = document.querySelector(".navbar")
 
 // Video sources for the backgrounds
 const backgroundVideos = [
@@ -13,10 +14,11 @@ const backgroundVideos = [
 gameElements.forEach((gameElement, index) => {
   gameElement.addEventListener("mouseenter", () => {
     overlayElements[index].classList.add("fade-in");
+
+    // navbar.classList.add("hidden")
     // Hide the other game elements
     gameElements.forEach((el, i) => {
       if (i != index) {
-        
         el.classList.add("hidden");
       }
     });
@@ -24,18 +26,83 @@ gameElements.forEach((gameElement, index) => {
 
   gameElement.addEventListener("mouseleave", () => {
     overlayElements[index].classList.remove("fade-in");
-    // Show the hidden game elements 
+    // navbar.classList.remove("hidden");
+    // Show the hidden game elements
     gameElements.forEach((el) => {
       el.classList.remove("hidden");
-
     });
   });
 
   // Set video source for each overlay
-  const video = document.createElement('video');
+  const video = document.createElement("video");
   video.src = backgroundVideos[index];
   video.autoplay = true;
   video.loop = true;
   video.muted = true;
   overlayElements[index].appendChild(video);
 });
+
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.4;
+    y += (nextCircle.y - y) * 0.4;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
